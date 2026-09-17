dd9d73d8ed596d926807177339791723b35438ac
NagesK <mohanabalanageswaran46@gmail.com>
2026-09-17 04:15:23 +0000

    Initial commit for FoodGo Jenkins project

---
+++ src/main/java/com/foodgo/App.java
@@ -0,0 +1,8 @@
+package com.foodgo;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to FoodGo!");
+        System.out.println("Jenkins CI/CD Demo");
+    }
+}
